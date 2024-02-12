da8119b482dfa6c3ebc5b4d14ea1c446818aedb0
pawelw1 <pawel.wlodarczyk@eliatra.com>
2024-02-12 17:53:29 +0000

    I've done it - Pawel
    Signed-off-by: pawelw1 <pawel.wlodarczyk@eliatra.com>

--- test-react-repository/src/components/No1.js
+++ test-react-repository/src/components/No1.js
@@ -1,6 +1,6 @@
 function No1() { 
     return(
-        <div>this is a test line in the code</div>
+        <div>this is a test line in the code - This is another website</div>
     )
 }
 
--- test-react-repository/src/components/No2.js
+++ test-react-repository/src/components/No2.js
@@ -1,6 +1,6 @@
 function No2() { 
     return(
-        <div>this is a second line in the code</div>
+        <div>this is a second line in the code - This link doesn't work at the moment</div>
     )
 }
 
